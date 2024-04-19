a5a8cc69e481ecfd9e2c540d753574d9ed1ca452
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-04-19 14:13:19 +0300

    first commit

---
+++ wp-content/themes/basic-theme/front-page.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+
+<?php get_template_part('templates/hero'); ?>
+<section class="section-content">
+    <?php the_content() ?>
+</section>
+
+<?php get_footer() ?>
---
+++ wp-content/themes/basic-theme/index.php
@@ -0,0 +1,54 @@
+<?php get_header() ?>
+<div class="page-container">
+
+    <section class="site-page" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
+                                ?>>
+        <div class="page-header__inner">
+            <div class="fixed-container">
+                 <ul class="breadcrumbs__list">
+                    <?php echo true_breadcrumbs(); 
+                    ?>
+                </ul> 
+                <h2 class="site-page__title toright"> <?php if (is_page('blog')){ echo 'Блог'; } ?> </h2>
+            </div>
+        </div>
+
+        <div class="site-page__content">
+            <div class="fixed-container">
+			
+                <?php
+                $args = array(
+                    'post_type' => 'books',
+                    'publish' => true,
+                    //'paged' => get_query_var('paged'),
+                );
+                query_posts($args);
+                if (have_posts()) : ?>
+                    <div class="archive-posts__grid books">
+                        <?php while (have_posts()) : the_post(); ?>
+                                <?php echo get_the_post_thumbnail() ?>
+
+                                <div class="entry-post__inner__head">
+                                    <h3><?php the_title(); ?></h3>
+                                    <div class="entry-post__excerpt"><?php the_excerpt() ?></div>
+                                </div>
+                        <?php endwhile; ?>
+                    </div>
+                <?php endif; ?>
+
+
+
+
+            </div>
+        </div>
+
+
+
+
+    </section>
+
+</div>
+
+
+
+<?php get_footer() ?>
